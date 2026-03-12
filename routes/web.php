525a1020e20432b161e219d8c6b744949af895ac
info <info@a-site.by>
2026-03-12 17:22:27 +0300

    work

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Index\IndexController;
 use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index'])->name('home.index'); // стандартный метод
 
-Route::group(['middleware' => ['role:manager']], function () {
-Route::get('/widget', \App\Http\Controllers\Widget\IndexController::class)->name('home.widget'); // однометодный контроллер
+    Route::group(['middleware' => ['role:manager']], function () {
 
-});
+        Route::get('/widget', \App\Http\Controllers\Widget\IndexController::class)->name('home.widget'); // однометодный контроллер
+
+    });
