7e9be282c4c5bda528e2e18833756508046b92f9
info <info@a-site.by>
2026-03-15 16:12:40 +0300

    work

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layout.main')
 @section('content')
 
-    Нужно зарегистрироваться и войти под этим пользователем. Новый пользователь автоматически получит права manager
+    <p>Нужно зарегистрироваться и войти под этим пользователем. Новый пользователь автоматически получит права manager</p>
+<p>http://localhost:8876/api/tickets - постом можно создать запрос. Документацию по этому методу может быть напишу позже в SWAGGER.</p>
+<p>http://localhost:8876/widget - форма для такой же заявки, но она снова идет на /api/tickets.</p>
 
-    http://localhost:8876/api/tickets - постом можно создать запрос. Документацию по этому методу может быть напишу позже в SWAGGER.
-    http://localhost:8876/api/tickets/statistics - список вопросов по API
-    http://localhost:8876/api/tickets/statistics?start_date=2023-01-01&end_date=2027-01-31 - фильтрация по дате
+<p>http://localhost:8876/api/tickets/statistics - список вопросов по API</p>
+<p>http://localhost:8876/api/tickets/statistics?period=day/month/week - фильтрация по периаоду</p>
 
 @endsection
